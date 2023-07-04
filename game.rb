e5e31712056b55dba2013ad0dc060a885f2bdb8b
Yerbol Kuldeyev <yerbol.kuldeyev@gmail.com>
2023-07-04 14:20:51 -0700

    add code to begin with

---
+++ game.rb
@@ -0,0 +1,9 @@
+class Game
+  def initialize
+    @player1 = Player.new
+    @player2 = Player.new
+    @current_player = @player1
+  end
+
+  
+end
---
+++ player.rb
@@ -0,0 +1,9 @@
+class Player
+  attr_reader :lives, :score
+
+  def initialize
+    @lives = 3
+    @score = 0
+  end
+
+end
